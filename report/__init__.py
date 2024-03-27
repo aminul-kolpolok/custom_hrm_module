a7173b282cd2f6ed7a2c1d3625d05cf977fd07c7
aminul124387@gmail.com <aminul124387@gmail.com>
2024-03-27 09:32:37 +0600

    Report Update

--- report/__init__.py
+++ report/__init__.py
@@ -1,3 +1,3 @@
-from . import attendance_report_wizard
-from . import employee_attendance_report
+# from . import attendance_report_wizard
+# from . import employee_attendance_report
 from . import hr_report
